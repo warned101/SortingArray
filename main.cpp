8e2d2bfa46f3eee0f6ab3af35cd1b84009c98eae
warned101 <32792774+warned101@users.noreply.github.com>
2017-11-15 19:11:46 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,36 +1,25 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
-int PARTITION(int arr[], int p, int r)
+void bubblesort(int arr[],int n)
 {
-    int temp;
-    int x=arr[r];
-    int i=p-1;
-    for(int j=p;j<r;j++)
+    int i,j,temp;
+    for(i=0;i<n-1;i++)
     {
-        if(arr[j]<=x)
+        for(j=0;j<n-i-1;j++)
         {
-            i=i+1;
-            temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
+            if(arr[j]>arr[j+1])
+                {
+                    temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
         }
-    }
-    temp=arr[i+1];
-    arr[i+1]=arr[r];
-    arr[r]=temp;
-    return (i+1);
-}
 
-void quicksort(int arr[], int p, int r)
-{
-    if(p<r)
-    {
-       int q=PARTITION(arr, p, r);
-       quicksort(arr,p,q-1);
-       quicksort(arr,q+1,r);
     }
+    for(i=0;i<n;i++)
+            cout << arr[i];
 }
 
 int main()
@@ -38,13 +27,11 @@ int main()
     int i,arr[100],n;
     cout << "Enter size of array: ";
     cin >> n;
-    int p=0;
-    int r=n-1;
     cout << "Enter the elements of array: " << endl;
     for(i=0;i<n;i++)
         cin >> arr[i];
-    quicksort(arr,p,r);
-    for(int j=0;j<n;j++)
-        cout << arr[j];
+    bubblesort(arr,n);
     return 0;
+
+
 }
